5546bd3c96dccedee03a1b46c15c9c7ef19a9194
Iyke24 <egbokasteve@yahoo.com>
2023-02-16 15:50:43 -0800

    Print different size in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	printf("Size of a Char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an Int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a Long Int:%zu byte(s)\n", sizeof(long int));
-	printf("Size of a Long long Int: %zu byte(s)", sizeof(long long int));
-	printf("Size of a Float: %zu byte(s)", sizeof(float));
+	printf("Size of a Long Int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a Long long Int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a Float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
