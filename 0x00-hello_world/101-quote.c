18b72f7c5a6906ec91dcf140c332014cd73b5db0
Iyke24 <egbokasteve@yahoo.com>
2023-02-16 16:18:41 -0800

    Using write to print ouput in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unstdio.h>
+
+/**
+ * main - Entry point
+ * Result: Always 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+
+	return (1);
+}
