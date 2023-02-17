5cb0977bc0b69d1e4895166af88f2f60d5ab1b46
Iyke24 <egbokasteve@yahoo.com>
2023-02-16 16:31:29 -0800

    Using write to print ouput in c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 
 /**
  * main - Entry point
- * Result: Always 1 (Success)
+ * Result: 1 if success
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
