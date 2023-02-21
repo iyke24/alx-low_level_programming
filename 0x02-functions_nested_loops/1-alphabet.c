0dad872b29694ba07495a02d72079f4e378ce08c
Iyke24 <egbokasteve@yahoo.com>
2023-02-21 15:29:04 -0800

    Function that prints all alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,8 @@
 
 /*
  * print_alphabet -  function to print all the alphabet in lowercase
- * Description: 'the function uses while loop to print the alphabet'
- * Return : Always 0 (success)
+ *
+ * Description: the function uses while loop to print the alphabet
  */
 
 void print_alphabet(void)
