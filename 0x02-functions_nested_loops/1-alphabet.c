3137328a52acdba29225ecc88a694727a2f027fe
Iyke24 <egbokasteve@yahoo.com>
2023-02-21 15:33:30 -0800

    Function that prints all alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * print_alphabet -  function to print all the alphabet in lowercase
  *
  * Description: the function uses while loop to print the alphabet
  */
-
 void print_alphabet(void)
 {
 	char alph;
