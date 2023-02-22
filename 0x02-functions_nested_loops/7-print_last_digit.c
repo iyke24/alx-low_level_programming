e2fdea6be69d9a9c1aadfd57d09266ed26b7756b
Iyke24 <egbokasteve@yahoo.com>
2023-02-21 20:02:10 -0800

    find the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,20 @@
 
 /**
  * print_last_digit - print last digit of integer
- * @n: integer
- * Return: last digit of number
+ * @nld: integer
+ * Return: last digit of a number
  */
-
-int print_last_digit(int n)
+int print_last_digit(int nld)
 {
-	if (n < 0)
-		n *= -1;
-	_putchar('0' + (n % 10));
+	int pld;
 
-	return (n % 10);
+	pld = (nld % 10);
+
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+
+	_putchar(pld + '0');
+	return (pld);
 }
