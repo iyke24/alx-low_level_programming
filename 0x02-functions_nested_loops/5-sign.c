742ea8ac050b50f23636f677b4ba78504d9860ed
Iyke24 <egbokasteve@yahoo.com>
2023-02-21 18:59:57 -0800

    check the sign of n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - print + if greater than zero, 0 if zero and - if less than zero
+ * Description: Check the sign of n
+ * @n: the variable parameter
+ * Return: +, 0 or -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		putchar('+');
+	return (1);
+	else if (n == 0)
+		putchar(0);
+	return (0);
+	else
+		putchar('-');
+	return (-1);
+}
