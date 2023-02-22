d6e76b6515f0cc5f923c337459baab04146ac9f6
Iyke24 <egbokasteve@yahoo.com>
2023-02-21 18:27:27 -0800

    Check the case of a variable return 1 if lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 #include <ctype.h>
+
 /**
  * _islower - print 1 if lowercase else 0
  * Description: check the case of the parameter
- * c: parameter variable to check the case
+ * @c: parameter variable to check the case
  * Return: 0 Or 1
  */
 int _islower(int c)
