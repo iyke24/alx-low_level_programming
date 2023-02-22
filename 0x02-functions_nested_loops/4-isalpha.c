051140a18c72abc713fcd3b14105a7b1edf33e4f
Iyke24 <egbokasteve@yahoo.com>
2023-02-21 18:44:52 -0800

    check if c is alphabet, lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * _isalpha - check if c is alphabet, lower or uppercase
  * Description: the function returns 1 if true else 0
  * @c: the parameter variable
- * Return: 1 or 0 
+ * Return: 1 or 0
  */
 int _isalpha(int c)
 {
