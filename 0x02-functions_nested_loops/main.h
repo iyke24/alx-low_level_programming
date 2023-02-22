c7730e5ee5503653463e082c8670e72eeaf3fc13
Iyke24 <egbokasteve@yahoo.com>
2023-02-21 20:09:20 -0800

    Print every minute of the day

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -57,3 +57,17 @@ int _abs(int);
  */
 
 int print_last_digit(int);
+
+/**
+ * jack_bauer - print every minute of a day
+ * Description: print minutes 00:00 to 23:59
+ */
+
+void jack_bauer(void);
+
+
+/**
+ * times_table - print multiplication table
+ */
+
+void times_table(void);
