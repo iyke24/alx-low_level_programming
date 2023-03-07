9c66f323a96dcd12e2aa139c25d301877c4c75d5
Iyke24 <egbokasteve@yahoo.com>
2023-03-07 11:26:06 -0800

    check and search two arrays

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,22 +6,18 @@
  * @accept: input
  * Return: Always 0 (Success)
  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n;
 
-char *_strpbrk(char *s, char *f){
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+				return (s + i);
+		}
+	}
 
-   int i;
-    int n;
-
-    for(i=0; s[i] !='\0'; i++){
-
-        for(n=0; f[n] !='\0'; n++){
-            if(s[i] == f[n])
-                return (s + i);
-
-
-        }
-    }
-
-                return (NULL);
-
+	return (NULL);
 }
