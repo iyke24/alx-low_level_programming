046c2f40dfaecf08c0e2a11d4261e9555629a176
Iyke24 <egbokasteve@yahoo.com>
2023-03-13 13:04:04 -0700

    Print all the argument it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,17 +2,19 @@
 #include "main.h"
 
 /**
- * main - print all the arguments passed to it
- * @argc: argument counter
- * @argv: pointer to arguments
- * Return: 0 on success
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	while(i < argc)
-		printf("%s\n", argv[i++]);
 
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 	return (0);
 }
