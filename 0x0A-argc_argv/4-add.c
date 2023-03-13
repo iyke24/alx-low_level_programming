945fb3c78e0597d18f88c0dd469e9a63f3158b4b
Iyke24 <egbokasteve@yahoo.com>
2023-03-13 13:30:36 -0700

    Add all the positive number passed in the argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while(i < argc)
+	{
+		if(!isdigit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
