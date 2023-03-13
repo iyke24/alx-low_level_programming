cc078bca967b4ce29c48b6bc2d03f49b56b36f56
Iyke24 <egbokasteve@yahoo.com>
2023-03-13 11:57:15 -0700

    print all the arguments passed to a function

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all the arguments passed to it
+ * @argc: argument counter
+ * @argv: pointer to arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while(i < argc)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
